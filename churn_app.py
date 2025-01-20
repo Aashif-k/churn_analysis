82afda6a6d09c8f85ad3be4b65e77e905966a373
Aashif-k <ashif.aachi.k@gmail.com>
2025-01-21 00:02:09 +0530

    churn_app.py

--- churn_app.py
+++ churn_app.py
@@ -50,13 +50,13 @@ def main():
            intl_mins,intl_calls,day_mins,day_charge,eve_mins
            ,eve_charge,night_mins,night_charge,customer_calls)
    st.markdown('''
-             <style>
-             .element-container {
-             background-color: blue;
-             opacity: 1;
-             }
-             </style>
-             ''', unsafe_allow_html=True)
+   <style>
+   .element-container {
+   background-color: blue;
+   opacity: 1;
+   }
+   </style>
+   ''', unsafe_allow_html=True)
   st.success('The output is {}'.format(result))
 if __name__=='__main__':
   main()
