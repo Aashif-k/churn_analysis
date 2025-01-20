4c05a48f6d0a466550c5a91bf715c4733c38e9fc
Aashif-k <ashif.aachi.k@gmail.com>
2025-01-20 23:43:46 +0530

    churn_app.py

--- churn_app.py
+++ churn_app.py
@@ -45,7 +45,7 @@ def main():
   customer_calls=st.number_input("customer.calls",step=1,min_value=0,max_value=9)
   result=""
   left, middle, right = st.columns(3)
-  if middle.button("predict"):
+  if left.button("predict"):
     result=predic(voice_plan,intl_plan,
            intl_mins,intl_calls,day_mins,day_charge,eve_mins
            ,eve_charge,night_mins,night_charge,customer_calls)
