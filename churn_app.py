31500ee79add27a26d9958461ebf41e5a180670d
Aashif-k <ashif.aachi.k@gmail.com>
2025-01-20 23:05:56 +0530

    churn_app.py

--- churn_app.py
+++ churn_app.py
@@ -4,9 +4,11 @@ import numpy as np
 import pickle
 page_pg_img="""
 <style>
+[data-testid="stAppViewContainer"]{
 background-color: #e5e5f7;
 opacity: 0.8;
 background-image:  repeating-radial-gradient( circle at 0 0, transparent 0, #e5e5f7 10px ), repeating-linear-gradient( #444cf755, #444cf7 );
+}
 </style>
 """
 pickle_in = open('xgb_model1.pkl','rb')
