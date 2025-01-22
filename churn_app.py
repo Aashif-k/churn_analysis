c5f61c5dc938a567af427495d464c72f1492a795
Aashif-k <ashif.aachi.k@gmail.com>
2025-01-22 23:37:11 +0530

    churn_app.py

--- churn_app.py
+++ churn_app.py
@@ -94,7 +94,7 @@ def main():
 
   for percent_complete in range(100):
       time.sleep(0.01)
-      my_bar.progress(percent_complete + 10, text=progress_text)
+      my_bar.progress(percent_complete + 1, text=progress_text)
       time.sleep(0.01)
       my_bar.empty()
   if m.button("predict"):
